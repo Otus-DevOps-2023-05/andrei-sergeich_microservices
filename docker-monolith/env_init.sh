8c8fe917a2907efe0b494053baaba23784939d02
Andrei Prokhorov <andrei-sergeich@yandex.ru>
2023-07-09 19:53:42 +0300

    Updated scripts

--- docker-monolith/env_init.sh
+++ docker-monolith/env_init.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# --generic-ip-address= ПОДСТАВИТЬ IP СОЗДАННОЙ VM
+VM_NAME=docker-host
+VM_SSH_USER=yc-user
+VM_IP_ADDRESS=$(yc compute instance show --name $VM_NAME | grep -E ' +address' | tail -n 1 | awk '{print $2}')
+SSH_KEY_PATH=~/.ssh/id_ed25519
+
 docker-machine create \
     --driver generic \
-    --generic-ip-address=158.160.106.174 \
-    --generic-ssh-user yc-user \
-    --generic-ssh-key ~/.ssh/id_ed25519 \
-    docker-host
+    --generic-ip-address=$VM_IP_ADDRESS \
+    --generic-ssh-user $VM_SSH_USER \
+    --generic-ssh-key $SSH_KEY_PATH \
+    $VM_NAME
